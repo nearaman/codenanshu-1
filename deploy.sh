f827978fbb6752f9fa0113d9df7befdcefe1d1c4
Anshu <anshumeena1947@gmail.com>
2021-07-17 15:39:04 +0530

    update deploy.dh

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,18 @@
 vared -p "Enter commit message: " -c message
 
 printf "\e[33m\nBuilding project...\e[39m\n"
-hugo -d ../YOUR_GITHUB_USERNAME.github.io
+hugo
 
-printf "\\e[33m\nPushing to YOUR_GITHUB_USERNAME.github.io repository...\e[39m\n\n"
-cd ../YOUR_GITHUB_USERNAME.github.io
+printf "\\e[33m\nPushing to the web...\e[39m\n\n"
+cd ../public
 git add .
 git commit -m "$message"
-git push origin master
+git push origin web
 printf "\e[32m\nSuccessfully deployed the website!\e[39m"
 
-printf "\e[33m\n\nNow pushing latest changes to PERSONAL_REPOSITORY_NAME repository...\e[39m\n\n"
-cd ../PERSONAL_REPOSITORY_NAME
+printf "\e[33m\n\nNow pushing latest changes to codenanshu repository...\e[39m\n\n"
+cd ../
 git add .
 git commit -m "$message"
-git push origin master
+git push origin main
 printf "\033[0;32m\nSuccessfully pushed changes to the repository!\e[39m\n"
