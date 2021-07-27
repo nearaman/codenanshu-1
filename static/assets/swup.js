2d7187d30f373bdd6fbfd20f79c957253ee0636a
Anshu Meena <Anshumeena1947@gmail.com>
2021-07-27 14:01:41 +0530

    add mouse animation and vim tutorial

--- static/assets/swup.js
+++ static/assets/swup.js
@@ -10,12 +10,26 @@ function openMenu() {
 }
 function closeMenu() {
   let width = screen.width;
-  if (width < 640){
-  var x = document.getElementById("menu");
-  if (x.className == "hidden") {
-    x.className = "show";
-  } else {
-    x.className = "hidden";
-  }
+  if (width < 640) {
+    var x = document.getElementById("menu");
+    if (x.className == "hidden") {
+      x.className = "show";
+    } else {
+      x.className = "hidden";
+    }
   }
 }
+
+let curs = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+  curs.style.left = x - 20 + "px";
+  curs.style.top = y - 20 + "px";
+  curs.style.opacity = "1";
+});
+
+document.addEventListener("click", (e) => {
+  curs.style.transform.translate = "scale(1.2)";
+});
