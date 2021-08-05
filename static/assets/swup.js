587361e2a465bd429cc559d268257b53d8683fd3
Anshu Meena <anshumeena1947@gmail.com>
2021-08-05 09:04:12 +0530

    remove git

--- static/assets/swup.js
+++ static/assets/swup.js
@@ -19,14 +19,3 @@ function closeMenu() {
     }
   }
 }
-function githubEdit(urlObject){
-  let stringURL = urlObject.toString();
-  if(stringURL === "https://codenanshu.in/"){
-    return "https://github.com/aianshume/codenanshu/";
-  } else {
-    return stringURL.replace("https://codenanshu.in/","https://github.com/aianshume/codenanshu/tree/main/content/").slice(0, -1).concat('.md');
-  }
-}
-
-let gitedit = document.getElementById("githubedit");
-gitedit.href = githubEdit(window.location);
