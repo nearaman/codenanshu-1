f6aeafba526d73db40d73e85f6f77f124501d7a4
aianshu <aianshume@yahoo.com>
2021-08-15 07:14:38 +0530

    add: animations

--- static/assets/swup.js
+++ static/assets/swup.js
@@ -19,18 +19,3 @@ function closeMenu() {
     }
   }
 }
-
-function rendomLottieAnimation(){
-  let animations = [
-    "https://assets10.lottiefiles.com/packages/lf20_w51pcehl.json",
-    "https://assets2.lottiefiles.com/packages/lf20_iorpbol0.json",
-    "https://assets10.lottiefiles.com/packages/lf20_9unpvaft.json",
-    "https://assets2.lottiefiles.com/datafiles/TwXatofJPhLwwn75v8AembjHGFUvPSoKeIJxrjPO/sky.json"
-  ]
-
-  let toSend = Math.floor(Math.random() * animations.length);
-  let player = document.getElementById("lottiePlayer");
-  player.src = animations[toSend];
-}
-
-rendomLottieAnimation();
