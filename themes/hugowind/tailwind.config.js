21e11229cefbdc486949696c64927e5ae1632294
Anshu <anshumeena1947@gmail.com>
2021-09-04 23:48:52 +0530

    refix font

--- themes/hugowind/tailwind.config.js
+++ themes/hugowind/tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
 
       fontFamily: {
         ...fontFamily,
-        'sans': '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif'
+        'sans': 'Varela Round, sans-serif'
       },
 
       typography: (theme) => ({
